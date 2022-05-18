6e7b0b47d20e9ddb8153ad095b8cfa496cbc7ae7
Laila Ribeiro Hamilton de Campos <laila.estagio@anatel.gov.br>
2022-05-18 18:18:33 -0300

    Install and set up django-heroku

--- employee_manager/settings.py
+++ employee_manager/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
+import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -136,3 +137,5 @@ REST_FRAMEWORK = {
         # 'rest_framework.authentication.TokenAuthentication'
     ]
 }
+
+django_heroku.settings(locals())
